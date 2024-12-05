cbc53a753bcf9a407df7e9b75b06ed3e1df7ee54
Filip Zymek <filip.zymek@withintent.com>
2024-12-05 13:10:47 +0100

    day 4, pt 1 and pt2

--- src/day_04/src/main.rs
+++ src/day_04/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{Read};
+use std::io::Read;
 
 fn main() {
     // read file into memory
@@ -25,12 +25,197 @@ fn main() {
     println!("Result for puzzle 2: {}", result_puzzle_2);
 }
 
+#[derive(Debug)]
+struct Element {
+    char: char,
+    x: usize,
+    y: usize
+}
 
 fn process_puzzle_1(input: &str) -> String {
-    return String::new();
+    let grid = create_grid(input);
+    // println!("{:?}", grid);
+
+   let result: i32 = grid.iter().map(|row|{
+    row.iter().map(|e| {
+        check_xmass(e, &grid)
+    })
+    .sum::<i32>()
+   })
+   .sum();
+
+    return result.to_string();
+}
+
+fn create_grid(input: &str) -> Vec<Vec<Element>> {
+    let mut elements: Vec<Vec<Element>> = Vec::new();
+    
+    for (y, line) in input.lines().enumerate() {
+        let mut row: Vec<Element> = Vec::new();
+        for (x, c) in line.chars().enumerate() {
+            row.push(Element { char: c, x: x, y: y });
+        }
+        elements.push(row);
+    }
+    elements
+}
+
+fn check_xmass(element: &Element, grid: &Vec<Vec<Element>>) -> i32 {
+    let grid_height = grid.len().try_into().unwrap();
+    let grid_width = grid[0].len().try_into().unwrap();
+
+    let mut num_xmass = 0;
+
+    // check north
+    if element.y as i32 - 3 >= 0 {
+        let mut text = String::new();
+        text.push(grid[element.y][element.x].char);
+        text.push(grid[element.y - 1][element.x].char);
+        text.push(grid[element.y - 2][element.x].char);
+        text.push(grid[element.y - 3][element.x].char);
+
+        if text == "XMAS" {
+            num_xmass += 1;
+        }
+    }
+
+    // check north-east
+    if element.y as i32 - 3 >= 0 && element.x + 3 < grid_width {
+        let mut text = String::new();
+        text.push(grid[element.y][element.x].char);
+        text.push(grid[element.y - 1][element.x + 1].char);
+        text.push(grid[element.y - 2][element.x + 2].char);
+        text.push(grid[element.y - 3][element.x + 3].char);
+
+        if text == "XMAS" {
+            num_xmass += 1;
+        }
+    }
+
+    // check east
+    if element.x + 3 < grid_width {
+        let mut text = String::new();
+        text.push(grid[element.y][element.x].char);
+        text.push(grid[element.y][element.x + 1].char);
+        text.push(grid[element.y][element.x + 2].char);
+        text.push(grid[element.y][element.x + 3].char);
+
+        if text == "XMAS" {
+            num_xmass += 1;
+        }
+    }
+
+    // check south-east
+    if element.y + 3 < grid_height && element.x + 3 < grid_width {
+        let mut text = String::new();
+        text.push(grid[element.y][element.x].char);
+        text.push(grid[element.y + 1][element.x + 1].char);
+        text.push(grid[element.y + 2][element.x + 2].char);
+        text.push(grid[element.y + 3][element.x + 3].char);
+
+        if text == "XMAS" {
+            num_xmass += 1;
+        }
+    }
+
+    // check south
+    if element.y + 3 < grid_height {
+        let mut text = String::new();
+        text.push(grid[element.y][element.x].char);
+        text.push(grid[element.y + 1][element.x].char);
+        text.push(grid[element.y + 2][element.x].char);
+        text.push(grid[element.y + 3][element.x].char);
+
+        if text == "XMAS" {
+            num_xmass += 1;
+        }
+    }
+
+    // check south-west
+    if element.y + 3 < grid_height && element.x as i32 - 3 >= 0 {
+        let mut text = String::new();
+        text.push(grid[element.y][element.x].char);
+        text.push(grid[element.y + 1][element.x - 1].char);
+        text.push(grid[element.y + 2][element.x - 2].char);
+        text.push(grid[element.y + 3][element.x - 3].char);
+
+        if text == "XMAS" {
+            num_xmass += 1;
+        }
+    }
+
+    // check west
+    if element.x as i32 - 3 >= 0 {
+        let mut text = String::new();
+        text.push(grid[element.y][element.x].char);
+        text.push(grid[element.y][element.x - 1].char);
+        text.push(grid[element.y][element.x - 2].char);
+        text.push(grid[element.y][element.x - 3].char);
+
+        if text == "XMAS" {
+            num_xmass += 1;
+        }
+    }
+
+    // check north-west
+
+    if element.y as i32 - 3 >= 0 && element.x as i32 - 3 >= 0 {
+        let mut text = String::new();
+        text.push(grid[element.y][element.x].char);
+        text.push(grid[element.y - 1][element.x - 1].char);
+        text.push(grid[element.y - 2][element.x - 2].char);
+        text.push(grid[element.y - 3][element.x - 3].char);
+
+        if text == "XMAS" {
+            num_xmass += 1;
+        }
+    }
+
+    num_xmass
 }
 
 
 fn process_puzzle_2(input: &str) -> String {
-    return String::new();
+
+    let grid = create_grid(input);
+
+    let result: i32 = grid.iter().map(|row|{
+        row.iter().map(|e| {
+            check_x_mas(e, &grid)
+        })
+        .sum::<i32>()
+    })
+    .sum();
+
+    return result.to_string();
+}
+
+fn check_x_mas(element: &Element, grid: &Vec<Vec<Element>>) -> i32 {
+    let grid_height = grid.len().try_into().unwrap();
+    let grid_width = grid[0].len().try_into().unwrap();
+
+    if element.char != 'A' {
+        return 0;
+    }
+
+    if element.x as i32 - 1 < 0 || element.x + 1 >= grid_width || element.y as i32 - 1 < 0 || element.y + 1 >= grid_height {
+        return 0;
+    }
+
+    let mut diag1 = String::new();
+    let mut diag2 = String::new();
+
+    diag1.push(grid[element.y - 1][element.x - 1].char);
+    diag1.push(grid[element.y][element.x].char);
+    diag1.push(grid[element.y + 1][element.x + 1].char);
+
+    diag2.push(grid[element.y - 1][element.x + 1].char);
+    diag2.push(grid[element.y][element.x].char);
+    diag2.push(grid[element.y + 1][element.x - 1].char);
+
+    if (diag1 == "MAS" || diag1 == "SAM") && (diag2 == "MAS" || diag2 == "SAM") {
+        return 1;
+    }
+    
+    0
 }
